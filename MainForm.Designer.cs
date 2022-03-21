b18db56141aee901491dc89acbd1534efb1ce7e6
Theodor Stana <stana@kth.se>
2022-03-21 21:48:12 +0100

    Change form title and make form non-sizable

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -498,10 +498,11 @@ namespace CalibBeamCtrl
             this.Controls.Add(this.groupboxConfig);
             this.Controls.Add(this.groupboxShutterCtrl);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Calibration Beam Control";
+            this.Text = "Calib Beam Ctrl";
             ((System.ComponentModel.ISupportInitialize)(this.spbRotationAngle)).EndInit();
             this.groupboxConfig.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.spbOnTimeSec)).EndInit();
